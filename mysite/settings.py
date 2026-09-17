ee6e8c1b73578ef47a3dec5e3db3c4f104cb6f0f
Animesh Miglani <animesh.miglani31@gmail.com>
2026-09-18 00:05:14 +0530

    Prepare Django app for deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,9 +30,18 @@ SECRET_KEY = os.environ.get(
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 
 ALLOWED_HOSTS = []
+
+
+if os.environ.get("RENDER"):
+    ALLOWED_HOSTS = [os.environ.get("RENDER_EXTERNAL_HOSTNAME")]
+
+CSRF_TRUSTED_ORIGINS = []
+
+
+    
 
 
 # Application definition
@@ -81,13 +90,14 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+import dj_database_url
 
+DATABASES = {
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.1/ref/settings/#auth-password-validators
